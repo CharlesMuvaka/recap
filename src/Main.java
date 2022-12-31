2aab6b68287558a07dd16a29b7d19776d28bdc6d
CharlesMuvaka <charles.uvaka@student.moringaschool.com>
2022-12-31 21:40:24 +0300

    initialise an instance of the spring core container.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pojos.Employee;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+                                 Spring core container
+            - provides interfaces that are used to manage the bean's lifecycle.
+                1. BeanFactory
+                2. ApplicationContext - An implementation of the BeanFactory interface.
+
+         */
+
+        //initializing a Spring core Container
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        //creating an object from a bean using the container
+        Employee one = (Employee) context.getBean("emp1");
+        System.out.println(one.getSalary());
     }
 }
